3b6dea66911d34a08521c1ba0b8055e0af96b9e2
zlzmm5188 <zlzmm5188@gmail.com>
2025-11-28 21:28:50 +0800

    Add cleanup script

---
+++ cleanup/cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Cleanup script
+# This script is intended to clean up temporary files and other unwanted files.
+
+# Add your cleanup commands below
+
+# Example: Remove all `.tmp` files
+# find . -type f -name '*.tmp' -delete
